bb51aae733c664f66da7041fc3bb61afafb6a925
Amazingandyyy <amazingandyyy@gmail.com>
2017-05-22 18:19:27 -0700

    brokenschema

--- schema/schema.js
+++ schema/schema.js
@@ -1,3 +1,45 @@
-import graphql from 'graphql';
-const {GraphQLObjectType} = graphql;
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} from 'graphql';
+import axios from 'axios';
 
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLString
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    type: UserType,
+    args: {
+      id: {
+        type: GraphQLString
+      }
+    },
+    resolve(parentValue, args) {
+      return axios
+        .get(`http://localhost:3000/users/${args.id}`)
+        .then(res => res.data);
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery
+});
+
+export default schema;
